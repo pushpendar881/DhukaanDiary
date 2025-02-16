b972e2d53602d039fb24ea460ccab53683076bd9
pushpendar881 <pushpendarchoudhary106@gmail.com>
2025-02-16 12:49:31 +0530

    LoginPage and SignupPage

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:dhukaan/Pages/loginpage.dart';
+import 'package:flutter/material.dart';
+// import 'package:dhukaan/Pages/SignupPage.dart';p
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:LoginPage(),
+   );
+  }
+}
